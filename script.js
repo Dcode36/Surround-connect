9ed1433196e30a9ffe003964e697d926f12d0b0d
Digvijay Kadam <digvi@Digvijays-MacBook-Air.local>
2023-05-18 22:28:57 +0530

    sudya chutiya

--- script.js
+++ script.js
@@ -1,119 +1,104 @@
-// Setting LocalStorage
+// Check if notes exist in localStorage
 if (!localStorage.notes) {
-    let newObj = []
-    localStorage.setItem('notes', JSON.stringify(newObj)) 
-}
-else {
-    showNotes()
+    let newObj = [];
+    localStorage.setItem('notes', JSON.stringify(newObj));
+} else {
+    showNotes();
 }
 
-// Submit and DeleteAll
+// Submit and DeleteAll buttons
 let submitBtn = document.querySelector('#submitBtn');
 let delAllBtn = document.querySelector('#delAllBtn');
 
+// Event listener for Post button
+submitBtn.addEventListener('click', createNewNote);
 
+// Create a new note
 function createNewNote() {
+    // Get input values
+    let title = document.querySelector('#title').value.trim();
+    let note = document.querySelector('#note').value.trim();
+    let date = document.querySelector('#date').value.trim();
+    let location = document.querySelector('#location').value.trim();
 
-    // Title
-    let title = document.querySelector('#title');
-    let titleValue = title.value;
-
-    // Note
-    let note = document.querySelector('#note');
-    let noteValue = note.value;
-
-    // date
-    let date = document.querySelector('#date');
-    let dateValue = date.value;
-    // location
-    let location = document.querySelector('#location');
-    let locValue = location.value;
-    // Checking if both title and notes value exist
-    if (titleValue == '') {
+    // Check if inputs are empty
+    if (title === '' || note === '' || date === '' || location === '') {
         return false;
     }
-    else if (noteValue == '') {
-        return false;
-    }
-    else if (dateValue == ''){
-        return false;
-    }
-    else if (locValue == ''){
-        return false;
-    }
-    // Creating New Note and pushing in localStorage
-    else {
-        let newObj = JSON.parse(localStorage.getItem('notes'))
-        let newNote = {
-            'title': titleValue.trim(),
-            'date' : dateValue.trim(),
-            'location': locValue.trim(),
-            'text': noteValue.trim()
-        }
-        newObj.push(newNote)
-        localStorage.setItem('notes', JSON.stringify(newObj))
-    }
 
-    // Showing the Notes after been created
+    // Create new note object
+    let newNote = {
+        'title': title,
+        'date': date,
+        'location': location,
+        'desc': note
+    };
 
-    // Clearing the Input Value
+    // Retrieve notes from localStorage
+    let newObj = JSON.parse(localStorage.getItem('notes')) || [];
+
+    // Push new note to notes array
+    newObj.push(newNote);
+
+    // Store updated notes in localStorage
+    localStorage.setItem('notes', JSON.stringify(newObj));
+
+    // Clear input values
     document.querySelector('#title').value = '';
     document.querySelector('#note').value = '';
     document.querySelector('#date').value = '';
     document.querySelector('#location').value = '';
-    showNotes()
 
+    // Display updated notes
+    showNotes();
 }
 
+// Show all notes
 function showNotes() {
-    // Cloning card element to update exsiting values 
-    document.querySelector('.row').innerHTML = ''
-    allNotes = JSON.parse(localStorage.getItem('notes'));
+    let parentNode = document.querySelector('.row');
+    parentNode.innerHTML = '';
+
+    // Retrieve notes from localStorage
+    let allNotes = JSON.parse(localStorage.getItem('notes'));
 
     allNotes.forEach((element, index) => {
         let clonedNode = document.getElementById('new-note').cloneNode(true);
-        let parentNode = document.querySelector('.row');
-        // Displaying Notes
-        let createNoteTitle = element['title'];
-        let createNoteText = element['text'];
-        let createDate = element['date'];
-        let createLocation = element['location'];
         clonedNode.id = `note-${index}`;
         clonedNode.style.display = 'block';
-        clonedNode.querySelector('h5').innerText = createNoteTitle;
-        clonedNode.querySelector('h6').innerText = createDate;
-        clonedNode.querySelector('p').innerText = createLocation;
-        clonedNode.querySelector('p').innerText = createNoteText;
-        parentNode.appendChild(clonedNode);
 
+        // Update note values
+        clonedNode.querySelector('h5').innerText = element.title;
+        clonedNode.querySelector('.card-date').innerText = element.date;
+        clonedNode.querySelector('.location').innerText = element.location;
+        clonedNode.querySelector('.card-text').innerText = element.desc;
+
+        // Add delete button functionality
+        clonedNode.querySelector('.delete-btn').addEventListener('click', () => {
+            delOneNote(index);
+        });
+
+        parentNode.appendChild(clonedNode);
     });
 }
 
-function delOneNote(event) {
-    parentElem = event.path[3]
+// Delete a single note
+function delOneNote(index) {
+    let newObj = JSON.parse(localStorage.getItem('notes'));
 
-    // Title
-    let title = parentElem.querySelector('h5').innerText;
+    newObj.splice(index, 1);
 
-    let newObj = JSON.parse(localStorage.getItem('notes'))
+    // Store updated notes in localStorage
+    localStorage.setItem('notes', JSON.stringify(newObj));
 
-    for( var i = 0; i < newObj.length; i++){ 
-        console.log(title);
-        if ( newObj[i]['title'] === title) { 
-            
-            console.log(i);
-            newObj.splice(i, 1); 
-        }
-    
-    }
-
-    localStorage.setItem('notes', JSON.stringify(newObj))
-    
-    showNotes()
+    // Display updated notes
+    showNotes();
 }
 
-function delAllNote(){
-    let newObj = []
-    localStorage.setItem('notes', JSON.stringify(newObj))
-    showNotes()
+// Delete all notes
+function delAllNote() {
+    let newObj = [];
+    localStorage.setItem('notes', JSON.stringify(newObj));
+
+    // Display updated notes
+    showNotes();
 }
